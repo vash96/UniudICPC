0a14783607d0167c704fd14c586d0e336f94e23e
Brian Riccardi <vash@localhost.localdomain>
2020-10-28 23:14:16 +0100

    Backtracking ez

---
+++ swerc/2013/VivoParc/brian_backtracking.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> graph;
+vector<int> colors;
+int n;
+
+bool backtrack(int u, int colored)
+{
+    if(colored==n) return true;
+    array<bool, 5> can = {true, true, true, true, true};
+    for(auto v : graph[u]) {
+        can[colors[v]] = false;
+    }
+    for(int c=1; c<=4; ++c) {
+        if(can[c]) {
+            colors[u] = c;
+            if(backtrack(u+1, colored+1))
+                return true;
+            colors[u] = 0;
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    scanf("%d\n", &n);
+
+    vector<vector<bool>> adj(n, vector<bool>(n, false));
+    int a, b;
+    while(scanf("%d-%d\n", &a, &b) != EOF) {
+        --a; --b;
+        adj[a][b] = adj[b][a] = true;
+    }
+
+    graph.resize(n);
+    colors.resize(n, 0);
+    for(int u=0; u<n-1; ++u) {
+        for(int v=u+1; v<n; ++v) {
+            if(adj[u][v]) {
+                graph[u].push_back(v);
+                graph[v].push_back(u);
+            }
+        }
+    }
+
+    backtrack(0, 0);
+
+    for(int i=0; i<n; ++i) {
+        printf("%d %d\n", i+1, colors[i]);
+    }
+
+    return 0;
+}
