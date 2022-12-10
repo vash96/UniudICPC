77f8eb45dff7109eec0e58f3d40ece77d8400dba
Brian Riccardi <brian.riccardi96@gmail.com>
2022-12-10 23:08:36 +0100

    +spiegazione in euclidsgame

--- kattis/euclidsgame/brian_logn.cpp
+++ kattis/euclidsgame/brian_logn.cpp
@@ -1,3 +1,20 @@
+/*
+Testo: https://open.kattis.com/problems/euclidsgame
+Tecnica: Game theory, Osservazione
+Complessità: O(log(n))
+
+Idea:
+ - Per semplicita' si consideri lo stato del gioco (a, b) con a >= b
+ - Sia k=floor(a/b), quindi k*b e' il massimo multiplo di b che posso giocare
+ - Se k=1, allora sono costretto a giocare b e l'avversario si trova nello stato (b, a-b)
+    (e la partita viene giocata ricorsivamente)
+ - Se k>1, allora posso sempre vincere:
+   -- Se k*b mi fa vincere, allora non c'e' altro da dimostrare
+   -- Se k*b mi fa perdere, allora lo stato (b, a-kb) e' vincente (per l'avversario),
+      ma allora mi basta giocare (k-1)*b, inducendo l'avversario in (a - (k-1)b, b) e costringendolo
+      a giocare b (unica mossa a disposizione), inducendo me nello stato (b, a-kb) che abbiamo stabilito essere vincente.
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
